c3a9b9f7ff08398cb9463014d75f845d1a9ccf53
Dereck Tu <dereck.tu@hotmail.com>
2019-09-14 23:05:41 -0400

    Get location

--- HTNProj/app/src/main/java/com/htn/proj/MainActivity.java
+++ HTNProj/app/src/main/java/com/htn/proj/MainActivity.java
@@ -1,16 +1,26 @@
 package com.htn.proj;
 //Test comment
+
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.textclassifier.TextLinks;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
@@ -27,23 +37,40 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,View.OnClickListener{
-    // nav bar
+import org.w3c.dom.Text;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private static final String[] LOCATION_PERMS = {
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    private static final int LOCATION_REQUEST = 1340;
     private DrawerLayout drawer;
+    private FusedLocationProviderClient client;
     public static double[] coord = new double[2];
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        client = LocationServices.getFusedLocationProviderClient(this);
+        if (!canAccessLocation()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(LOCATION_PERMS, LOCATION_REQUEST);
+            }
+        }
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         drawer = findViewById(R.id.drawer_layout);
         //reference to navigation view
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -53,6 +80,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_map);
         }
+        scheduler.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                checkLocation();
+                sendRequest();
+            }
+        }, 0, 5, TimeUnit.SECONDS);
+    }
+
+    public void sendRequest(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = String.format("http://167.99.191.137/polls/download?x=%f&y=%f&count=%d",coord[0],coord[1],10);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.getMessage());
+            }
+        });
+        queue.add(stringRequest);
     }
 
     @Override
@@ -77,28 +128,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new InfoFragment()).commit();
                 break;
             case R.id.nav_share:
-                LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-                LocationListener locationListener = new GeoListener();
-                try {
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
-                    System.out.println("die");
-                }catch(SecurityException e){};
-
-                RequestQueue queue = Volley.newRequestQueue(this);
-                String url = "http://localhost:8000/polls/download?x=";
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url + Math.round(coord[0]) + "&y=" + Math.round(coord[1]) + "&count=10", new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        System.out.println("Response: "+response);
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.println(error.getMessage());
-
-                    }
-                });
-                queue.add(stringRequest);
                 Intent sendIntent = new Intent();
                 sendIntent.setAction(Intent.ACTION_SEND);
                 sendIntent.putExtra(Intent.EXTRA_TEXT, "Hey meet me at: " + coord[0] + ", " + coord[1]);
@@ -107,16 +136,33 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Intent shareIntent = Intent.createChooser(sendIntent, null);
                 startActivity(shareIntent);
 
-//                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
                 break;
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    private void checkLocation(){
+        client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                System.out.println(location);
+                if(location!=null) {
+                    coord[0] = location.getLongitude();
+                    coord[1] = location.getLatitude();
+                    Toast.makeText(getApplicationContext(),"Long: "+coord[0]+"\nLat: "+coord[1],Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
-    @Override
-    public void onClick(View v) {
-
+    private boolean canAccessLocation() {
+        return(hasPermission(Manifest.permission.ACCESS_FINE_LOCATION));
+    }
+    private boolean hasPermission(String perm) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return (PackageManager.PERMISSION_GRANTED == checkSelfPermission(perm));
+        }
+        return false;
     }
     /*
              NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
