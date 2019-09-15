1cf1cc0e04c10fe8c28e43cb2fd75a860f6bbcee
advenia <august.lin1320@gmail.com>
2019-09-15 06:48:06 -0400

    fix folder paths

--- HTNProj/app/src/main/java/com/htn/proj/MapFragment.java
+++ HTNProj/app/src/main/java/com/htn/proj/MapFragment.java
@@ -36,8 +36,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        LatLng htn = new LatLng(MainActivity.coord[0],MainActivity.coord[1]);
+//        LatLng htn = new LatLng(MainActivity.coord[0],MainActivity.coord[1]);
+        LatLng htn = new LatLng(43.473223,-80.546191);
         map.addMarker(new MarkerOptions().position(htn).title("Hack the North"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(htn));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(htn, 15.0f));
     }
 }
